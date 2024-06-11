a96ff48e32de2d5c333e30845411a600f57b2f1a
mx <m.obrubov@yandex.ru>
2024-06-12 00:30:51 +0200

    addow using templates

--- src/ts/downfall.ts
+++ src/ts/downfall.ts
@@ -14,7 +14,9 @@ export interface DownfallColumn {
 
 export type DownfallElement = HTMLDivElement | HTMLImageElement;
 
-
+// NOTE: or image url or data and template to set
+export type DownfallTemplateData = { data: { [key: string]: any}, template: HTMLTemplateElement }
+export type DownfallSource = string | DownfallTemplateData;
 
 export class Downfall {
 
@@ -52,20 +54,34 @@ export class Downfall {
     this.parent.style.gap = this.options.gap;
   }
 
-  async add(_sources: string[]) {
+  async add(_sources: DownfallSource[]) {
     const sources = _sources.slice();
     const toAdd = sources.shift();
     const currentColumn = this.columns.reduce((prev, next, i) => {
       return prev.threshold <= next.threshold ? prev : next;
     });
 
-    await this._createImage(toAdd).then((image) => {
-      currentColumn.node.appendChild(image);
-      currentColumn.threshold += image.offsetHeight;
-      // TODO: trigger opacity transition after element creation  
-      // window.getComputedStyle(image).opacity;
-      image.classList.add(this.SHOWN_KLASS);
-    });
+    if (typeof toAdd === "string") {
+      await this._createImage(toAdd).then((image) => {
+        currentColumn.node.appendChild(image);
+        currentColumn.threshold += image.offsetHeight;
+        // TODO: trigger opacity transition after element creation  
+        // window.getComputedStyle(image).opacity;
+        image.classList.add(this.SHOWN_KLASS);
+      });
+    } else {
+      const fragment = this._createFragment(toAdd);
+      const element = fragment.firstElementChild as HTMLElement;
+
+      currentColumn.node.appendChild(fragment);
+
+      await this._waitForImages(element).then(_ => {
+        currentColumn.threshold += element.offsetHeight;
+        element.classList.add(this.SHOWN_KLASS);
+      });
+
+    }
+
 
     if (sources.length) this.add(sources);
   }
@@ -95,6 +111,37 @@ export class Downfall {
       }
       img.src = src;
     });
+  }
+
+  _createFragment(templateData: DownfallTemplateData): DocumentFragment {
+    const { data, template } = templateData;
+    const clone = template.content.cloneNode(true) as DocumentFragment;
+
+    Object.keys(data).forEach(key => {
+      const targets = clone.querySelectorAll(`[data-template-${key}]`);
+      Array.from(targets).forEach(t => {
+        if (t.tagName === "IMG") {
+          (t as HTMLImageElement).src = data[key];
+        } else {
+          t.textContent = data[key];
+        }
+      });
+    });
+
+    return clone;
+  }
+
+  _waitForImages(node: HTMLElement): Promise<HTMLImageElement[]> {
+    const images = Array.from(node.querySelectorAll("img"));
+    const promises: Promise<HTMLImageElement>[] = images.map(img => new Promise((resolve, reject) => {
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(new Error("Image load was broken"));
+    }));
+    return Promise.all(promises);
+  }
+
+  _waitForImage() {
+
   }
 
 
